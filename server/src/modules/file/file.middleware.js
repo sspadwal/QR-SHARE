bc27a958a33d2421af2da78c613b3fe21bdd4a11
sspadwal <sspadwal1002@gmail.com>
2026-05-24 11:12:29 +0530

    fixed session-id issue

--- server/src/modules/file/file.middleware.js
+++ server/src/modules/file/file.middleware.js
@@ -1,7 +1,8 @@
 import { validateSession } from '../session/session.service.js';
 
 const tokenCheck = async (req, res, next) => {
-    const sessionId = req.cookies.sessionId || req.headers['x-session-id'];
+    // Prioritize explicit header 'x-session-id' (passed by the React app) over cookies (which can be stale)
+    const sessionId = req.headers['x-session-id'] || req.cookies.sessionId;
 
     if (!sessionId) {
         return res.status(401).json({ message: 'Session id is Required' });
